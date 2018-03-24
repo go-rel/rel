7414ee43b18d1f39fa090ad8a87185ba586651dc
Muhammad Surya Asriadie <surya.asriadie@gmail.com>
2018-03-25 00:17:42 +0700

    updated repo

--- repo.go
+++ repo.go
@@ -4,6 +4,16 @@ type Repo struct {
 	adapter Adapter
 }
 
+func New(adapter Adapter) Repo {
+	return Repo{
+		adapter: adapter,
+	}
+}
+
+func (repo Repo) Close() error {
+	return repo.adapter.Close()
+}
+
 func (repo Repo) From(collection string) Query {
 	return Query{
 		Collection: collection,

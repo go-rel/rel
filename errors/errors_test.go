c0acea157da8337c5579ac967700931f05d0c81c
Muhammad Surya Asriadie <surya.asriadie@gmail.com>
2018-03-26 23:19:39 +0700

    test for errors

---
+++ errors/errors_test.go
@@ -0,0 +1,75 @@
+package errors
+
+import (
+	e "errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestErrorInterface(t *testing.T) {
+	assert.Equal(t, "error", error(Error{Message: "error"}).Error())
+}
+
+func TestNew(t *testing.T) {
+	assert.Equal(t, Error{
+		Message: "error",
+		Field:   "error",
+		Code:    1000,
+	}, New("error", "error", 1000))
+}
+
+func TestUnexpectedError(t *testing.T) {
+	err := UnexpectedError("error")
+
+	assert.Equal(t, "error", err.Error())
+	assert.Equal(t, "", err.Field)
+	assert.True(t, err.UnexpectedError())
+}
+
+func TestNotFoundError(t *testing.T) {
+	err := NotFoundError("error")
+
+	assert.Equal(t, "error", err.Error())
+	assert.Equal(t, "", err.Field)
+	assert.True(t, err.NotFoundError())
+}
+
+func TestChangesetError(t *testing.T) {
+	err := ChangesetError("error", "field")
+
+	assert.Equal(t, "error", err.Error())
+	assert.Equal(t, "field", err.Field)
+	assert.True(t, err.ChangesetError())
+}
+
+func TestDuplicateError(t *testing.T) {
+	err := DuplicateError("error", "field")
+
+	assert.Equal(t, "error", err.Error())
+	assert.Equal(t, "field", err.Field)
+	assert.True(t, err.DuplicateError())
+}
+
+func TestAssociationError(t *testing.T) {
+	err := AssociationError("error", "field")
+
+	assert.Equal(t, "error", err.Error())
+	assert.Equal(t, "field", err.Field)
+	assert.True(t, err.AssociationError())
+}
+
+func TestWrap(t *testing.T) {
+	assert.Equal(t, nil, Wrap(nil))
+	assert.Equal(t, Error{}, Wrap(Error{}))
+	assert.Equal(t, UnexpectedError("error"), Wrap(e.New("error")))
+}
+
+func TestMultiError(t *testing.T) {
+	var errs error = Errors{
+		UnexpectedError("error1"),
+		NotFoundError("error2"),
+	}
+
+	assert.Equal(t, "error1, error2", errs.Error())
+}

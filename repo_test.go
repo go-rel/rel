85d603b79117af8c76d51dfe76cc50c51ced6f4c
Muhammad Surya Asriadie <surya.asriadie@gmail.com>
2018-03-27 00:16:08 +0700

    transaction test

--- repo_test.go
+++ repo_test.go
@@ -3,8 +3,11 @@ package grimoire
 import (
 	"testing"
 
+	"github.com/Fs02/grimoire/errors"
 	"github.com/stretchr/testify/assert"
 )
+
+type User struct{}
 
 var repo = Repo{}
 
@@ -13,4 +16,90 @@ func TestFrom(t *testing.T) {
 		Collection: "users",
 		Fields:     []string{"*"},
 	})
+}
+
+func TestTransaction(t *testing.T) {
+	mock := new(TestAdapter)
+	mock.On("Begin").Return(nil).
+		On("Commit").Return(nil)
+
+	err := Repo{adapter: mock}.Transaction(func(r Repo) error {
+		// doing good things
+		return nil
+	})
+
+	assert.Nil(t, err)
+	mock.AssertExpectations(t)
+}
+
+func TestTransactionBeginError(t *testing.T) {
+	mock := new(TestAdapter)
+	mock.On("Begin").Return(errors.UnexpectedError("error"))
+
+	err := Repo{adapter: mock}.Transaction(func(r Repo) error {
+		// doing good things
+		return nil
+	})
+
+	assert.Equal(t, errors.UnexpectedError("error"), err)
+	mock.AssertExpectations(t)
+}
+
+func TestTransactionCommitError(t *testing.T) {
+	mock := new(TestAdapter)
+	mock.On("Begin").Return(nil).
+		On("Commit").Return(errors.UnexpectedError("error"))
+
+	err := Repo{adapter: mock}.Transaction(func(r Repo) error {
+		// doing good things
+		return nil
+	})
+
+	assert.Equal(t, errors.UnexpectedError("error"), err)
+	mock.AssertExpectations(t)
+}
+
+func TestTransactionReturnErrorAndRollback(t *testing.T) {
+	mock := new(TestAdapter)
+	mock.On("Begin").Return(nil).
+		On("Rollback").Return(nil)
+
+	err := Repo{adapter: mock}.Transaction(func(r Repo) error {
+		// doing good things
+		return errors.UnexpectedError("error")
+	})
+
+	assert.Equal(t, errors.UnexpectedError("error"), err)
+	mock.AssertExpectations(t)
+}
+
+func TestTransactionPanicWithKnownErrorAndRollback(t *testing.T) {
+	mock := new(TestAdapter)
+	mock.On("Begin").Return(nil).
+		On("Rollback").Return(nil)
+
+	err := Repo{adapter: mock}.Transaction(func(r Repo) error {
+		// doing good things
+		panic(errors.NotFoundError("error"))
+		return nil
+	})
+
+	assert.Equal(t, errors.NotFoundError("error"), err)
+	mock.AssertExpectations(t)
+}
+
+func TestTransactionPanicAndRollback(t *testing.T) {
+	mock := new(TestAdapter)
+	mock.On("Begin").Return(nil).
+		On("Rollback").Return(nil)
+
+	assert.Panics(t, func() {
+		Repo{adapter: mock}.Transaction(func(r Repo) error {
+			// doing good things
+			panic(errors.UnexpectedError("error"))
+			return nil
+		})
+	})
+
+	mock.AssertExpectations(t)
 }

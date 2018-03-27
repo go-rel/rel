de26ac8af25b5112b295ee5bb606b0f634eaa9c8
Muhammad Surya Asriadie <surya.asriadie@gmail.com>
2018-03-27 21:27:00 +0700

    added test for exec and query

--- adapter/mysql/mysql_test.go
+++ adapter/mysql/mysql_test.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"testing"
+	"time"
 
 	"github.com/Fs02/grimoire/errors"
 	"github.com/go-sql-driver/mysql"
@@ -42,6 +43,73 @@ func TestTransactionRollbackError(t *testing.T) {
 	defer adapter.Close()
 
 	assert.NotNil(t, adapter.Rollback())
+}
+
+func TestQuery(t *testing.T) {
+	adapter := new(Adapter)
+	adapter.Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
+	defer adapter.Close()
+
+	out := struct{}{}
+
+	// normal query
+	count, err := adapter.Query(&out, "SELECT 10;", []interface{}{})
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), count)
+
+	// within transaction
+	assert.Nil(t, adapter.Begin())
+
+	count, err = adapter.Query(&out, "SELECT 10;", []interface{}{})
+	assert.Nil(t, err)
+
+	assert.Equal(t, int64(1), count)
+	assert.Nil(t, adapter.Commit())
+}
+
+func TestQueryError(t *testing.T) {
+	adapter := new(Adapter)
+	adapter.Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
+	defer adapter.Close()
+
+	out := struct{}{}
+
+	_, err := adapter.Query(&out, ";;", []interface{}{})
+	assert.NotNil(t, err)
+}
+
+func TestExec(t *testing.T) {
+	adapter := new(Adapter)
+	adapter.Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
+	defer adapter.Close()
+
+	// normal exec
+	stmt := "INSERT INTO users (name, created_at, updated_at) VALUES (?,?,?)"
+	args := []interface{}{"find", time.Now().Round(time.Second), time.Now().Round(time.Second)}
+	id, count, err := adapter.Exec(stmt, args)
+
+	assert.Nil(t, err)
+	assert.True(t, id > 0)
+	assert.Equal(t, int64(1), count)
+
+	// within transaction
+	assert.Nil(t, adapter.Begin())
+
+	id, count, err = adapter.Exec(stmt, args)
+	assert.Nil(t, err)
+	assert.True(t, id > 0)
+	assert.Equal(t, int64(1), count)
+
+	assert.Nil(t, adapter.Commit())
+}
+
+func TestExecError(t *testing.T) {
+	adapter := new(Adapter)
+	adapter.Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
+	defer adapter.Close()
+
+	_, _, err := adapter.Exec(";;", []interface{}{})
+	assert.NotNil(t, err)
 }
 
 func TestError(t *testing.T) {

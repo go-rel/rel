98437b9ea537c8355efddb9d2e1388946838b6b3
Muhammad Surya Asriadie <surya.asriadie@gmail.com>
2018-03-29 22:16:27 +0700

    use packaged snakecase

--- adapter/sqlutil/scan.go
+++ adapter/sqlutil/scan.go
@@ -3,7 +3,8 @@ package sqlutil
 import (
 	"database/sql"
 	"reflect"
-	"unicode"
+
+	"github.com/azer/snakecase"
 )
 
 // minimum rows interface for test purpose
@@ -100,26 +101,9 @@ func fieldIndex(rt reflect.Type) map[string]int {
 
 			fields[tag] = i
 		} else {
-			fields[toSnake(f.Name)] = i
+			fields[snakecase.SnakeCase(f.Name)] = i
 		}
 	}
 
 	return fields
-}
-
-// convert string to snake case
-// https://gist.github.com/elwinar/14e1e897fdbe4d3432e1
-func toSnake(in string) string {
-	runes := []rune(in)
-	length := len(runes)
-
-	var out []rune
-	for i := 0; i < length; i++ {
-		if i > 0 && unicode.IsUpper(runes[i]) && ((i+1 < length && unicode.IsLower(runes[i+1])) || unicode.IsLower(runes[i-1])) {
-			out = append(out, '_')
-		}
-		out = append(out, unicode.ToLower(runes[i]))
-	}
-
-	return string(out)
 }

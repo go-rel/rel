03d4fb66e8940bac92112d49031033f599182013
Muhammad Surya Asriadie <surya.asriadie@gmail.com>
2018-04-04 00:39:48 +0700

    added test for transaction

--- adapter/mysql/adapter_test.go
+++ adapter/mysql/adapter_test.go
@@ -15,12 +15,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type Address struct {
+	ID      int64
+	UserID  int64
+	Address string
+}
+
 type User struct {
 	ID        int64
 	Name      string
 	Gender    string
 	Age       int
 	Note      *string
+	Addresses []Address
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
@@ -268,10 +275,75 @@ func TestRepoDelete(t *testing.T) {
 	assert.True(t, id > 0)
 	assert.Equal(t, int64(1), count)
 
-	err = grimoire.New(adapter).From("users").Find(id).Delete()
+	repo := grimoire.New(adapter)
+	err = repo.From("users").Find(id).Delete()
 	assert.Nil(t, err)
 
 	user := User{}
-	err = grimoire.New(adapter).From("users").Find(id).One(&user)
+	err = repo.From("users").Find(id).One(&user)
 	assert.NotNil(t, err)
+}
+
+func changeUser(user interface{}, params map[string]interface{}) *changeset.Changeset {
+	ch := changeset.Cast(user, params, []string{
+		"name",
+		"gender",
+		"age",
+		"note",
+	})
+	changeset.CastAssoc(ch, "addresses", changeAddress)
+	return ch
+}
+
+func changeAddress(address interface{}, params map[string]interface{}) *changeset.Changeset {
+	ch := changeset.Cast(address, params, []string{"address"})
+	return ch
+}
+
+func TestRepoTransaction(t *testing.T) {
+	adapter, err := Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer adapter.Close()
+	repo := grimoire.New(adapter)
+
+	user := User{}
+
+	params := map[string]interface{}{
+		"name":   "whiteviolet",
+		"gender": "male",
+		"age":    18,
+		"note":   "some note here",
+		"addresses": []map[string]interface{}{
+			{
+				"address": "Aceh, Indonesia",
+			},
+			{
+				"address": "Bandung, Indonesia",
+			},
+		},
+	}
+
+	ch := changeUser(user, params)
+	assert.Nil(t, ch.Error())
+
+	err = repo.Transaction(func(repo grimoire.Repo) error {
+		repo.From("users").MustInsert(&user, ch)
+		addresses := ch.Changes()["addresses"].([]*changeset.Changeset)
+		repo.From("addresses").Set("user_id", user.ID).MustInsert(&user.Addresses, addresses...)
+
+		return nil
+	})
+
+	assert.Nil(t, err)
+	assert.True(t, user.ID > 0)
+	assert.Equal(t, "whiteviolet", user.Name)
+	assert.Equal(t, "male", user.Gender)
+	assert.Equal(t, 18, user.Age)
+	assert.Equal(t, "some note here", *user.Note)
+
+	assert.Equal(t, 2, len(user.Addresses))
+	assert.Equal(t, "Aceh, Indonesia", user.Addresses[0].Address)
+	assert.Equal(t, "Bandung, Indonesia", user.Addresses[1].Address)
 }

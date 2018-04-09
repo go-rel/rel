c8ff54a5b00592067ae260b3c7c7f378e0c0a919
Muhammad Surya Asriadie <surya.asriadie@gmail.com>
2018-04-09 23:29:17 +0700

    init sqlite3 support

---
+++ adapter/sqlite3/sqlite3_test.go
@@ -0,0 +1,167 @@
+package sqlite3
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Fs02/go-paranoid"
+	"github.com/Fs02/grimoire"
+	"github.com/Fs02/grimoire/adapter/specs"
+	"github.com/Fs02/grimoire/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	adapter, err := Open(dsn())
+	if err != nil {
+		panic(err)
+	}
+	defer adapter.Close()
+
+	_, _, err = adapter.Exec(`DROP TABLE IF EXISTS addresses;`, []interface{}{})
+	paranoid.Panic(err)
+	_, _, err = adapter.Exec(`DROP TABLE IF EXISTS users;`, []interface{}{})
+	paranoid.Panic(err)
+
+	_, _, err = adapter.Exec(`CREATE TABLE users (
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(30),
+		gender VARCHAR(10),
+		age INTEGER,
+		note varchar(50),
+		created_at DATETIME,
+		updated_at DATETIME
+	);`, []interface{}{})
+	paranoid.Panic(err)
+
+	_, _, err = adapter.Exec(`CREATE TABLE addresses (
+		id INTEGER PRIMARY KEY,
+		user_id INTEGER,
+		address VARCHAR(60),
+		created_at DATETIME,
+		updated_at DATETIME,
+		FOREIGN KEY (user_id) REFERENCES users(id)
+	);`, []interface{}{})
+	paranoid.Panic(err)
+}
+
+func dsn() string {
+	if os.Getenv("SQLITE3_DATABASE") != "" {
+		return os.Getenv("SQLITE3_DATABASE")
+	}
+
+	return "./grimoire_test.db"
+}
+
+func TestSpecs(t *testing.T) {
+	adapter, err := Open(dsn())
+	if err != nil {
+		panic(err)
+	}
+	defer adapter.Close()
+	repo := grimoire.New(adapter)
+
+	// Query Specs
+	specs.Query(t, repo)
+	specs.QueryJoin(t, repo)
+	specs.QueryNotFound(t, repo)
+
+	// Insert Specs
+	specs.Insert(t, repo)
+	specs.InsertAll(t, repo)
+	specs.InsertSet(t, repo)
+
+	// Update Specs
+	specs.Update(t, repo)
+	specs.UpdateWhere(t, repo)
+	specs.UpdateSet(t, repo)
+
+	// Put Specs
+	specs.SaveInsert(t, repo)
+	specs.SaveInsertAll(t, repo)
+	specs.SaveUpdate(t, repo)
+
+	// Delete specs
+	specs.Delete(t, repo)
+
+	// Transaction specs
+	specs.Transaction(t, repo)
+}
+
+func TestAdapterInsertAllError(t *testing.T) {
+	adapter, err := Open(dsn())
+	if err != nil {
+		panic(err)
+	}
+	defer adapter.Close()
+
+	fields := []string{"notexist"}
+	allchanges := []map[string]interface{}{
+		{"notexist": "12"},
+		{"notexist": "13"},
+	}
+
+	_, err = adapter.InsertAll(grimoire.Repo{}.From("users"), fields, allchanges)
+
+	assert.NotNil(t, err)
+}
+
+func TestAdapterTransactionCommitError(t *testing.T) {
+	adapter, err := Open(dsn())
+	if err != nil {
+		panic(err)
+	}
+	defer adapter.Close()
+
+	assert.NotNil(t, adapter.Commit())
+}
+
+func TestAdapterTransactionRollbackError(t *testing.T) {
+	adapter, err := Open(dsn())
+	if err != nil {
+		panic(err)
+	}
+	defer adapter.Close()
+
+	assert.NotNil(t, adapter.Rollback())
+}
+
+func TestAdapterQueryError(t *testing.T) {
+	adapter, err := Open(dsn())
+	if err != nil {
+		panic(err)
+	}
+	defer adapter.Close()
+
+	out := struct{}{}
+
+	_, err = adapter.Query(&out, "error", []interface{}{})
+	assert.NotNil(t, err)
+}
+
+func TestAdapterExecError(t *testing.T) {
+	adapter, err := Open(dsn())
+	if err != nil {
+		panic(err)
+	}
+	defer adapter.Close()
+
+	_, _, err = adapter.Exec("error", []interface{}{})
+	assert.NotNil(t, err)
+}
+
+func TestAdapterError(t *testing.T) {
+	adapter := new(Adapter)
+
+	// error nil
+	assert.Nil(t, adapter.Error(nil))
+
+	// TODO: 1062 error
+	// rawerr := &mysql.MySQLError{Message: "duplicate", Number: 1062}
+	// duperr := errors.DuplicateError(rawerr.Message, "")
+	// assert.Equal(t, duperr, adapter.Error(rawerr))
+
+	// other errors
+	err := errors.UnexpectedError("error")
+	assert.Equal(t, err, adapter.Error(err))
+}

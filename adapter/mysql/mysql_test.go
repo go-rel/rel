254993690f27db058020255607fa8f5ff06f64e1
Muhammad Surya Asriadie <surya.asriadie@gmail.com>
2018-04-09 22:06:51 +0700

    added transaction specs

--- adapter/mysql/mysql_test.go
+++ adapter/mysql/mysql_test.go
@@ -36,10 +36,13 @@ func TestSpecs(t *testing.T) {
 	// Put Specs
 	specs.SaveInsert(t, repo)
 	specs.SaveInsertAll(t, repo)
-	specs.PutUpdate(t, repo)
+	specs.SaveUpdate(t, repo)
 
-	//Delete specs
+	// Delete specs
 	specs.Delete(t, repo)
+
+	// Transaction specs
+	specs.Transaction(t, repo)
 }
 
 func TestAdapterInsertAllError(t *testing.T) {
@@ -58,30 +61,6 @@ func TestAdapterInsertAllError(t *testing.T) {
 	_, err = adapter.InsertAll(grimoire.Repo{}.From("users"), fields, allchanges)
 
 	assert.NotNil(t, err)
-}
-
-func TestAdapterTransactionCommit(t *testing.T) {
-	adapter, err := Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
-	if err != nil {
-		panic(err)
-	}
-	defer adapter.Close()
-
-	txAdapter, err := adapter.Begin()
-	assert.Nil(t, err)
-	assert.Nil(t, txAdapter.Commit())
-}
-
-func TestAdapterTransactionRollback(t *testing.T) {
-	adapter, err := Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
-	if err != nil {
-		panic(err)
-	}
-	defer adapter.Close()
-
-	txAdapter, err := adapter.Begin()
-	assert.Nil(t, err)
-	assert.Nil(t, txAdapter.Rollback())
 }
 
 func TestAdapterTransactionCommitError(t *testing.T) {
@@ -104,83 +83,29 @@ func TestAdapterTransactionRollbackError(t *testing.T) {
 	assert.NotNil(t, adapter.Rollback())
 }
 
-// func TestAdapterQuery(t *testing.T) {
-// 	adapter, err := Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer adapter.Close()
+func TestAdapterQueryError(t *testing.T) {
+	adapter, err := Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer adapter.Close()
 
-// 	out := struct{}{}
+	out := struct{}{}
 
-// 	// normal query
-// 	count, err := adapter.Query(&out, "SELECT 10;", []interface{}{})
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, int64(1), count)
+	_, err = adapter.Query(&out, ";;", []interface{}{})
+	assert.NotNil(t, err)
+}
 
-// 	// within transaction
-// 	txAdapter, err := adapter.Begin()
-// 	assert.Nil(t, err)
+func TestAdapterExecError(t *testing.T) {
+	adapter, err := Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer adapter.Close()
 
-// 	count, err = txAdapter.Query(&out, "SELECT 10;", []interface{}{})
-// 	assert.Nil(t, err)
-
-// 	assert.Equal(t, int64(1), count)
-// 	assert.Nil(t, txAdapter.Commit())
-// }
-
-// func TestAdapterQueryError(t *testing.T) {
-// 	adapter, err := Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer adapter.Close()
-
-// 	out := struct{}{}
-
-// 	_, err = adapter.Query(&out, ";;", []interface{}{})
-// 	assert.NotNil(t, err)
-// }
-
-// func TestAdapterExec(t *testing.T) {
-// 	adapter, err := Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer adapter.Close()
-
-// 	// normal exec
-// 	stmt := "INSERT INTO users (name, created_at, updated_at) VALUES (?,?,?)"
-// 	args := []interface{}{"find", time.Now().Round(time.Second), time.Now().Round(time.Second)}
-// 	id, count, err := adapter.Exec(stmt, args)
-
-// 	assert.Nil(t, err)
-// 	assert.True(t, id > 0)
-// 	assert.Equal(t, int64(1), count)
-
-// 	// within transaction
-// 	// within transaction
-// 	txAdapter, err := adapter.Begin()
-// 	assert.Nil(t, err)
-
-// 	id, count, err = txAdapter.Exec(stmt, args)
-// 	assert.Nil(t, err)
-// 	assert.True(t, id > 0)
-// 	assert.Equal(t, int64(1), count)
-
-// 	assert.Nil(t, txAdapter.Commit())
-// }
-
-// func TestAdapterExecError(t *testing.T) {
-// 	adapter, err := Open(dsn() + "?charset=utf8&parseTime=True&loc=Local")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer adapter.Close()
-
-// 	_, _, err = adapter.Exec(";;", []interface{}{})
-// 	assert.NotNil(t, err)
-// }
+	_, _, err = adapter.Exec(";;", []interface{}{})
+	assert.NotNil(t, err)
+}
 
 func TestAdapterError(t *testing.T) {
 	adapter := new(Adapter)
---
+++ adapter/specs/transaction.go
@@ -0,0 +1,122 @@
+package specs
+
+import (
+	"testing"
+
+	"github.com/Fs02/grimoire"
+	"github.com/Fs02/grimoire/changeset"
+	"github.com/Fs02/grimoire/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+var params = map[string]interface{}{
+	"name":   "whiteviolet",
+	"gender": "male",
+	"age":    18,
+	"note":   "some note here",
+	"addresses": []map[string]interface{}{
+		{
+			"address": "Aceh, Indonesia",
+		},
+		{
+			"address": "Bandung, Indonesia",
+		},
+	},
+}
+
+// Transaction tests insert specifications.
+func Transaction(t *testing.T, repo grimoire.Repo) {
+	tests := []struct {
+		name  string
+		block func(*testing.T) func(grimoire.Repo) error
+		err   error
+	}{
+		{"QueryAll", queryAll, nil},
+		{"InsertWithAssoc", insertWithAssoc, nil},
+		{"InsertWithAssocError", insertWithAssocError, errors.NotFoundError("let's rollback")},
+		{"InsertWithAssocPanic", insertWithAssocPanic, errors.NotFoundError("let's rollback")},
+	}
+
+	for _, tt := range tests {
+		t.Run("Transaction|"+tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.err, repo.Transaction(tt.block(t)))
+		})
+	}
+}
+
+func queryAll(t *testing.T) func(repo grimoire.Repo) error {
+	users := []User{}
+
+	// transaction block
+	return func(repo grimoire.Repo) error {
+		repo.From("users").All(&users)
+
+		return nil
+	}
+}
+
+func insertWithAssoc(t *testing.T) func(repo grimoire.Repo) error {
+	user := User{}
+
+	ch := changeUser(user, params)
+	assert.Nil(t, ch.Error())
+
+	// transaction block
+	return func(repo grimoire.Repo) error {
+		repo.From("users").MustInsert(&user, ch)
+		addresses := ch.Changes()["addresses"].([]*changeset.Changeset)
+		repo.From("addresses").Set("user_id", user.ID).MustInsert(&user.Addresses, addresses...)
+
+		return nil
+	}
+}
+
+func insertWithAssocError(t *testing.T) func(repo grimoire.Repo) error {
+	user := User{}
+
+	ch := changeUser(user, params)
+	assert.Nil(t, ch.Error())
+
+	// transaction block
+	return func(repo grimoire.Repo) error {
+		repo.From("users").MustInsert(&user, ch)
+		addresses := ch.Changes()["addresses"].([]*changeset.Changeset)
+		repo.From("addresses").Set("user_id", user.ID).MustInsert(&user.Addresses, addresses...)
+
+		// should rollback
+		return errors.NotFoundError("let's rollback")
+	}
+}
+
+func insertWithAssocPanic(t *testing.T) func(repo grimoire.Repo) error {
+	user := User{}
+
+	ch := changeUser(user, params)
+	assert.Nil(t, ch.Error())
+
+	// transaction block
+	return func(repo grimoire.Repo) error {
+		repo.From("users").MustInsert(&user, ch)
+		addresses := ch.Changes()["addresses"].([]*changeset.Changeset)
+		repo.From("addresses").Set("user_id", user.ID).MustInsert(&user.Addresses, addresses...)
+
+		// should rollback
+		panic(errors.NotFoundError("let's rollback"))
+	}
+}
+
+func changeUser(user interface{}, params map[string]interface{}) *changeset.Changeset {
+	ch := changeset.Cast(user, params, []string{
+		"name",
+		"gender",
+		"age",
+		"note",
+	})
+	changeset.CastAssoc(ch, "addresses", changeAddress)
+	return ch
+}
+
+func changeAddress(address interface{}, params map[string]interface{}) *changeset.Changeset {
+	ch := changeset.Cast(address, params, []string{"address"})
+	return ch
+}

991d01e3f30b5acc57a8f699d9e7465bcf40f943
Muhammad Surya <surya.asriadie@gmail.com>
2019-02-10 11:37:43 +0700

    fix join

--- join/join.go
+++ join/join.go
@@ -4,42 +4,42 @@ import (
 	"github.com/Fs02/grimoire/query"
 )
 
-func Join(collection string) query.Join {
-	return query.JoinWith("JOIN", collection, "", "")
+func Join(collection string) query.JoinClause {
+	return query.Join(collection)
 }
 
-func On(collection string, from string, to string) query.Join {
-	return query.JoinWith("JOIN", collection, from, to)
+func On(collection string, from string, to string) query.JoinClause {
+	return query.JoinOn(collection, from, to)
 }
 
-func Inner(collection string) query.Join {
-	return query.JoinInnerOn(collection, "", "")
+func Inner(collection string) query.JoinClause {
+	return query.InnerJoin(collection)
 }
 
-func InnerOn(collection string, from string, to string) query.Join {
-	return query.JoinWith("INNER JOIN", collection, from, to)
+func InnerOn(collection string, from string, to string) query.JoinClause {
+	return query.InnerJoinOn(collection, from, to)
 }
 
-func Left(collection string) query.Join {
-	return query.JoinLeftOn(collection, "", "")
+func Left(collection string) query.JoinClause {
+	return query.LeftJoin(collection)
 }
 
-func LeftOn(collection string, from string, to string) query.Join {
-	return query.JoinWith("LEFT JOIN", collection, from, to)
+func LeftOn(collection string, from string, to string) query.JoinClause {
+	return query.LeftJoinOn(collection, from, to)
 }
 
-func Right(collection string) query.Join {
-	return query.JoinRightOn(collection, "", "")
+func Right(collection string) query.JoinClause {
+	return query.RightJoin(collection)
 }
 
-func RightOn(collection string, from string, to string) query.Join {
-	return query.JoinWith("RIGHT JOIN", collection, from, to)
+func RightOn(collection string, from string, to string) query.JoinClause {
+	return query.RightJoinOn(collection, from, to)
 }
 
-func Full(collection string) query.Join {
-	return query.JoinFullOn(collection, "", "")
+func Full(collection string) query.JoinClause {
+	return query.FullJoin(collection)
 }
 
-func FullOn(collection string, from string, to string) query.Join {
-	return query.JoinWith("FULL JOIN", collection, from, to)
+func FullOn(collection string, from string, to string) query.JoinClause {
+	return query.FullJoinOn(collection, from, to)
 }

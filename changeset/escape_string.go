52b143a1522876d24ca3b3b0b32feb2fcc846aea
kevinalfianto <kevinalfianto@gmail.com>
2019-03-28 13:51:09 +0700

    Add Function Escape String All (#54)
    * add escape string all
    
    * change Escape string into variadic variable
    
    * fix test

--- changeset/escape_string.go
+++ changeset/escape_string.go
@@ -5,6 +5,8 @@ import (
 )
 
 // EscapeString escapes special characters like "<" to become "&lt;". this is helper for html.EscapeString
-func EscapeString(ch *Changeset, field string) {
-	ApplyString(ch, field, html.EscapeString)
+func EscapeString(ch *Changeset, fields ...string) {
+	for _, field := range fields {
+		ApplyString(ch, field, html.EscapeString)
+	}
 }

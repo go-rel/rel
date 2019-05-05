e597c6852e1b1e1f05fba7ad097dcc18af5bc7b3
Muhammad Surya <surya.asriadie@gmail.com>
2019-05-05 12:11:31 +0700

    extract to schema package

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -1,4 +1,4 @@
-package internal
+package schema
 
 import (
 	"reflect"
@@ -6,6 +6,30 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
+
+type CustomSchema struct {
+	UUID  string
+	Price int
+}
+
+func (c CustomSchema) TableName() string {
+	return "_users"
+}
+
+func (c CustomSchema) PrimaryKey() (string, interface{}) {
+	return "_uuid", c.UUID
+}
+
+func (c CustomSchema) Fields() map[string]int {
+	return map[string]int{
+		"_uuid":  0,
+		"_price": 1,
+	}
+}
+
+func (c CustomSchema) Types() []reflect.Type {
+	return []reflect.Type{String, Int}
+}
 
 func TestSchema(t *testing.T) {
 	var (

b07d625aa6bc71f2de324173501ba0cad10069ff
Muhammad Surya <surya.asriadie@gmail.com>
2019-09-24 21:21:37 +0700

    optimize isZero, avoid allocation from using reflect.DeepEqual

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package grimoire
 
 import (
+	"math"
 	"reflect"
 )
 
@@ -73,8 +74,45 @@ func isZero(value interface{}) bool {
 	case isZeroer:
 		zero = v.IsZero()
 	default:
-		zero = reflect.DeepEqual(value, reflect.Zero(reflect.TypeOf(v)).Interface())
+		zero = isDeepZero(reflect.ValueOf(value))
 	}
 
 	return zero
+}
+
+// modified from https://golang.org/src/reflect/value.go?s=33807:33835#L1077
+func isDeepZero(rv reflect.Value) bool {
+	switch rv.Kind() {
+	case reflect.Bool:
+		return !rv.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return rv.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return rv.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return math.Float64bits(rv.Float()) == 0
+	case reflect.Complex64, reflect.Complex128:
+		c := rv.Complex()
+		return math.Float64bits(real(c)) == 0 && math.Float64bits(imag(c)) == 0
+	case reflect.Array:
+		for i := 0; i < rv.Len(); i++ {
+			if !isDeepZero(rv.Index(i)) {
+				return false
+			}
+		}
+		return true
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice, reflect.UnsafePointer:
+		return rv.IsNil()
+	case reflect.String:
+		return rv.Len() == 0
+	case reflect.Struct:
+		for i := 0; i < rv.NumField(); i++ {
+			if !isDeepZero(rv.Field(i)) {
+				return false
+			}
+		}
+		return true
+	default:
+		return true
+	}
 }

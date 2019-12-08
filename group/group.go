9f2e4d1b311fbbc4ea5313d1ffcf502419178459
Muhammad Surya <surya.asriadie@gmail.com>
2019-12-08 21:23:22 +0700

    use variable alias for sugared functions

--- group/group.go
+++ group/group.go
@@ -5,10 +5,7 @@ import (
 	"github.com/Fs02/rel"
 )
 
-func By(fields ...string) rel.GroupQuery {
-	return rel.NewGroup(fields...)
-}
-
-func Fields(fields ...string) rel.GroupQuery {
-	return By(fields...)
-}
+var (
+	By     = rel.NewGroup
+	Fields = rel.NewGroup
+)
--- join/join.go
+++ join/join.go
@@ -5,42 +5,15 @@ import (
 	"github.com/Fs02/rel"
 )
 
-func Join(collection string) rel.JoinQuery {
-	return rel.NewJoin(collection)
-}
-
-func On(collection string, from string, to string) rel.JoinQuery {
-	return rel.NewJoinOn(collection, from, to)
-}
-
-func Inner(collection string) rel.JoinQuery {
-	return rel.NewInnerJoin(collection)
-}
-
-func InnerOn(collection string, from string, to string) rel.JoinQuery {
-	return rel.NewInnerJoinOn(collection, from, to)
-}
-
-func Left(collection string) rel.JoinQuery {
-	return rel.NewLeftJoin(collection)
-}
-
-func LeftOn(collection string, from string, to string) rel.JoinQuery {
-	return rel.NewLeftJoinOn(collection, from, to)
-}
-
-func Right(collection string) rel.JoinQuery {
-	return rel.NewRightJoin(collection)
-}
-
-func RightOn(collection string, from string, to string) rel.JoinQuery {
-	return rel.NewRightJoinOn(collection, from, to)
-}
-
-func Full(collection string) rel.JoinQuery {
-	return rel.NewFullJoin(collection)
-}
-
-func FullOn(collection string, from string, to string) rel.JoinQuery {
-	return rel.NewFullJoinOn(collection, from, to)
-}
+var (
+	Join    = rel.NewJoin
+	On      = rel.NewJoinOn
+	Inner   = rel.NewInnerJoin
+	InnerOn = rel.NewInnerJoinOn
+	Left    = rel.NewLeftJoin
+	LeftOn  = rel.NewLeftJoinOn
+	Right   = rel.NewRightJoin
+	RightOn = rel.NewRightJoinOn
+	Full    = rel.NewFullJoin
+	FullOn  = rel.NewFullJoinOn
+)

f28f11b85dc508062facf21781dc768ef69d2b62
Muhammad Surya <surya.asriadie@gmail.com>
2020-01-19 11:53:10 +0700

    apply has many for map

--- map.go
+++ map.go
@@ -8,10 +8,16 @@ import (
 // This allows inserting or updating only on specified field.
 // Insert/Update of has one or belongs to can be done using other Map as a value.
 // Insert/Update of has many can be done using slice of Map as a value.
+// Map is intended to be used internally within application, and not to be exposed directly as an APIs.
 type Map map[string]interface{}
 
 // Apply modification.
 func (m Map) Apply(doc *Document, modification *Modification) {
+	var (
+		pField = doc.PrimaryField()
+		pValue = doc.PrimaryValue()
+	)
+
 	for field, value := range m {
 		switch v := value.(type) {
 		case Map:
@@ -31,18 +37,20 @@ func (m Map) Apply(doc *Document, modification *Modification) {
 			modification.SetAssoc(field, assocModification)
 		case []Map:
 			var (
-				mods   = make([]Modification, len(v))
-				assoc  = doc.Association(field)
-				col, _ = assoc.Collection()
+				assoc = doc.Association(field)
+				mods  = applyMaps(v, assoc)
 			)
 
-			col.Reset()
-
-			for i := range v {
-				mods[i] = Apply(col.Add(), v[i])
-			}
 			modification.SetAssoc(field, mods...)
 		default:
+			if field == pField {
+				if v != pValue {
+					panic(fmt.Sprint("rel: replacing primary value (", pValue, " become ", v, ") is not allowed"))
+				} else {
+					continue
+				}
+			}
+
 			if !doc.SetValue(field, v) {
 				panic(fmt.Sprint("rel: cannot assign ", v, " as ", field, " into ", doc.Table()))
 			}
@@ -50,4 +58,57 @@ func (m Map) Apply(doc *Document, modification *Modification) {
 			modification.SetValue(field, v)
 		}
 	}
+}
+
+func applyMaps(maps []Map, assoc Association) []Modification {
+	var (
+		mods    = make([]Modification, len(maps))
+		col, _  = assoc.Collection()
+		pField  = col.PrimaryField()
+		pIndex  = make(map[interface{}]int)
+		pValues = col.PrimaryValue().([]interface{})
+	)
+
+	for i, v := range pValues {
+		pIndex[v] = i
+	}
+
+	var (
+		curr    = 0
+		inserts []Map
+	)
+
+	for _, m := range maps {
+		if pChange, changed := m[pField]; changed {
+			// update
+			pID, ok := pIndex[pChange]
+			if !ok {
+				panic("rel: cannot update has many assoc that is not loaded or doesn't belong to this record")
+			}
+
+			if pID != curr {
+				col.Swap(pID, curr)
+				pValues[pID], pValues[curr] = pValues[curr], pValues[pID]
+			}
+
+			mods[curr] = Apply(col.Get(curr), m)
+			delete(pIndex, pChange)
+			curr++
+		} else {
+			inserts = append(inserts, m)
+		}
+	}
+
+	// delete stales
+	if curr < col.Len() {
+		// TODO: add to stale ids
+		col.Truncate(0, curr)
+	}
+
+	// inserts remaining
+	for i, m := range inserts {
+		mods[curr+i] = Apply(col.Add(), m)
+	}
+
+	return mods
 }
--- map_test.go
+++ map_test.go
@@ -11,33 +11,83 @@ func TestMap(t *testing.T) {
 		user User
 		doc  = NewDocument(&user)
 		data = Map{
-			"id":   1,
 			"name": "Luffy",
 			"age":  20,
 			"transactions": []Map{
-				{"id": 1, "item": "Sword"},
-				{"id": 2, "item": "Shield"},
+				{"item": "Sword"},
+				{"item": "Shield"},
 			},
 			"address": Map{
-				"id":     1,
 				"street": "Grove Street",
 			},
 		}
 		userModification = Apply(NewDocument(&User{}),
-			Set("id", 1),
 			Set("name", "Luffy"),
 			Set("age", 20),
 		)
 		transaction1Modification = Apply(NewDocument(&Transaction{}),
-			Set("id", 1),
 			Set("item", "Sword"),
 		)
 		transaction2Modification = Apply(NewDocument(&Transaction{}),
-			Set("id", 2),
 			Set("item", "Shield"),
 		)
 		addressModification = Apply(NewDocument(&Address{}),
-			Set("id", 1),
+			Set("street", "Grove Street"),
+		)
+	)
+
+	userModification.SetAssoc("transactions", transaction1Modification, transaction2Modification)
+	userModification.SetAssoc("address", addressModification)
+
+	assertModification(t, userModification, Apply(doc, data))
+	assert.Equal(t, User{
+		Name: "Luffy",
+		Age:  20,
+		Transactions: []Transaction{
+			{Item: "Sword"},
+			{Item: "Shield"},
+		},
+		Address: Address{
+			Street: "Grove Street",
+		},
+	}, user)
+}
+
+func TestMap_update(t *testing.T) {
+	var (
+		user = User{
+			ID: 1,
+			Transactions: []Transaction{
+				{ID: 2},
+				{ID: 3},
+			},
+			Address: Address{
+				ID: 4,
+			},
+		}
+		doc  = NewDocument(&user)
+		data = Map{
+			"name": "Luffy",
+			"age":  20,
+			"transactions": []Map{
+				{"id": 2, "item": "Sword"},
+				{"id": 3, "item": "Shield"},
+			},
+			"address": Map{
+				"street": "Grove Street",
+			},
+		}
+		userModification = Apply(NewDocument(&User{}),
+			Set("name", "Luffy"),
+			Set("age", 20),
+		)
+		transaction1Modification = Apply(NewDocument(&Transaction{}),
+			Set("item", "Sword"),
+		)
+		transaction2Modification = Apply(NewDocument(&Transaction{}),
+			Set("item", "Shield"),
+		)
+		addressModification = Apply(NewDocument(&Address{}),
 			Set("street", "Grove Street"),
 		)
 	)
@@ -51,12 +101,67 @@ func TestMap(t *testing.T) {
 		Name: "Luffy",
 		Age:  20,
 		Transactions: []Transaction{
-			{ID: 1, Item: "Sword"},
-			{ID: 2, Item: "Shield"},
+			{ID: 2, Item: "Sword"},
+			{ID: 3, Item: "Shield"},
 		},
 		Address: Address{
-			ID:     1,
+			ID:     4,
 			Street: "Grove Street",
 		},
 	}, user)
+}
+
+func TestMap_hasManyUpdateDeleteInsert(t *testing.T) {
+	var (
+		user = User{
+			Transactions: []Transaction{
+				{ID: 2},
+				{ID: 3},
+			},
+		}
+		doc  = NewDocument(&user)
+		data = Map{
+			"transactions": []Map{
+				{"item": "Sword"},
+				{"id": 3, "item": "Shield"},
+			},
+		}
+		userModification         = Apply(NewDocument(&User{}))
+		transaction1Modification = Apply(NewDocument(&Transaction{}),
+			Set("item", "Sword"),
+		)
+		transaction2Modification = Apply(NewDocument(&Transaction{}),
+			Set("item", "Shield"),
+		)
+	)
+
+	userModification.SetAssoc("transactions", transaction2Modification, transaction1Modification)
+
+	assertModification(t, userModification, Apply(doc, data))
+	assert.Equal(t, User{
+		Transactions: []Transaction{
+			{ID: 3, Item: "Shield"},
+			{Item: "Sword"},
+		},
+	}, user)
+}
+
+func TestMap_hasManyUpdateNotLoaded(t *testing.T) {
+	var (
+		user = User{
+			Transactions: []Transaction{
+				{ID: 2},
+			},
+		}
+		doc  = NewDocument(&user)
+		data = Map{
+			"transactions": []Map{
+				{"id": 3, "item": "Sword"},
+			},
+		}
+	)
+
+	assert.PanicsWithValue(t, "rel: cannot update has many assoc that is not loaded or doesn't belong to this record", func() {
+		Apply(doc, data)
+	})
 }

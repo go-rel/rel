b56ee88dfed41cd9c5877c796844794f35e411a9
Muhammad Surya <surya.asriadie@gmail.com>
2020-03-14 22:59:10 +0700

    improved test

--- reltest/find_and_count_all_test.go
+++ reltest/find_and_count_all_test.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"testing"
 
+	"github.com/Fs02/rel"
 	"github.com/Fs02/rel/where"
 	"github.com/stretchr/testify/assert"
 )
@@ -17,21 +18,22 @@ func TestFindAndCountAll(t *testing.T) {
 			{ID: 1, Title: "Golang for dummies"},
 			{ID: 2, Title: "Rel for dummies"},
 		}
+		query = rel.Where(where.Like("title", "%dummies%")).Limit(10).Offset(10)
 	)
 
-	repo.ExpectFindAndCountAll(where.Like("title", "%dummies%")).Result(books, 2)
+	repo.ExpectFindAndCountAll(query).Result(books, 12)
 
-	count, err := repo.FindAndCountAll(context.TODO(), &result, where.Like("title", "%dummies%"))
+	count, err := repo.FindAndCountAll(context.TODO(), &result, query)
 	assert.Nil(t, err)
-	assert.Equal(t, 2, count)
+	assert.Equal(t, 12, count)
 	assert.Equal(t, books, result)
 	repo.AssertExpectations(t)
 
-	repo.ExpectFindAndCountAll(where.Like("title", "%dummies%")).Result(books, 2)
+	repo.ExpectFindAndCountAll(query).Result(books, 12)
 	assert.NotPanics(t, func() {
-		count := repo.MustFindAndCountAll(context.TODO(), &result, where.Like("title", "%dummies%"))
+		count := repo.MustFindAndCountAll(context.TODO(), &result, query)
 		assert.Equal(t, books, result)
-		assert.Equal(t, 2, count)
+		assert.Equal(t, 12, count)
 	})
 	repo.AssertExpectations(t)
 }
@@ -44,19 +46,20 @@ func TestFindAndCountAll_error(t *testing.T) {
 			{ID: 1, Title: "Golang for dummies"},
 			{ID: 2, Title: "Rel for dummies"},
 		}
+		query = rel.Where(where.Like("title", "%dummies%")).Limit(10).Offset(10)
 	)
 
-	repo.ExpectFindAndCountAll(where.Like("title", "%dummies%")).ConnectionClosed()
+	repo.ExpectFindAndCountAll(query).ConnectionClosed()
 
-	count, err := repo.FindAndCountAll(context.TODO(), &result, where.Like("title", "%dummies%"))
+	count, err := repo.FindAndCountAll(context.TODO(), &result, query)
 	assert.Equal(t, sql.ErrConnDone, err)
 	assert.Equal(t, 0, count)
 	assert.NotEqual(t, books, result)
 	repo.AssertExpectations(t)
 
-	repo.ExpectFindAndCountAll(where.Like("title", "%dummies%")).ConnectionClosed()
+	repo.ExpectFindAndCountAll(query).ConnectionClosed()
 	assert.Panics(t, func() {
-		repo.MustFindAndCountAll(context.TODO(), &result, where.Like("title", "%dummies%"))
+		repo.MustFindAndCountAll(context.TODO(), &result, query)
 	})
 
 	assert.NotEqual(t, books, result)

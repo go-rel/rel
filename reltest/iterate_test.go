ad0dc517b9fe5f025f63ef6c117c70bf7ced4025
Muhammad Surya <surya.asriadie@gmail.com>
2020-04-04 20:39:03 +0700

    improve tests

--- reltest/iterate_test.go
+++ reltest/iterate_test.go
@@ -10,30 +10,61 @@ import (
 )
 
 func TestIterate(t *testing.T) {
-	var (
-		book  Book
-		repo  = New()
-		query = rel.From("users")
-	)
-
-	repo.ExpectIterate(query).Result([]Book{{ID: 1}, {ID: 2}})
-
-	count := 0
-	it := repo.Iterate(context.TODO(), query)
-	defer it.Close()
-	for {
-		if err := it.Next(&book); err == io.EOF {
-			break
-		} else {
-			assert.Nil(t, err)
-		}
-
-		assert.NotEqual(t, 0, book.ID)
-		count++
+	tests := []struct {
+		name   string
+		result interface{}
+		count  int
+	}{
+		{
+			name:   "struct",
+			result: Book{ID: 1},
+			count:  1,
+		},
+		{
+			name:   "struct pointer",
+			result: &Book{ID: 1},
+			count:  1,
+		},
+		{
+			name:   "slice",
+			result: []Book{{ID: 1}, {ID: 2}},
+			count:  2,
+		},
+		{
+			name:   "slice pointer",
+			result: &[]Book{{ID: 1}, {ID: 2}},
+			count:  2,
+		},
 	}
 
-	assert.Equal(t, 2, count)
-	repo.AssertExpectations(t)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var (
+				book  Book
+				repo  = New()
+				query = rel.From("users")
+			)
+
+			repo.ExpectIterate(query).Result(test.result)
+
+			count := 0
+			it := repo.Iterate(context.TODO(), query)
+			defer it.Close()
+			for {
+				if err := it.Next(&book); err == io.EOF {
+					break
+				} else {
+					assert.Nil(t, err)
+				}
+
+				assert.NotEqual(t, 0, book.ID)
+				count++
+			}
+
+			assert.Equal(t, test.count, count)
+			repo.AssertExpectations(t)
+		})
+	}
 }
 
 func TestIterate_single(t *testing.T) {

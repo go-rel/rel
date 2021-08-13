662331257a6008dc4f2095ad6b87ccb33045dfbf
Fs02 <surya.asriadie@gmail.com>
2021-08-14 02:17:20 +0900

    rewrite find and count all

--- reltest/find_and_count_all.go
+++ reltest/find_and_count_all.go
@@ -1,43 +1,57 @@
 package reltest
 
 import (
-	"fmt"
+	"context"
 	"reflect"
 
 	"github.com/go-rel/rel"
-	"github.com/stretchr/testify/mock"
 )
 
-// FindAndCountAll asserts and simulate find all function for test.
-type FindAndCountAll struct {
-	*Expect
+type findAndCountAll []*MockFindAndCountAll
+
+func (fca *findAndCountAll) register(ctxData ctxData, queriers ...rel.Querier) *MockFindAndCountAll {
+	mfca := &MockFindAndCountAll{ctxData: ctxData, argQuery: rel.Build("", queriers...)}
+	*fca = append(*fca, mfca)
+	return mfca
+}
+
+func (fca findAndCountAll) execute(ctx context.Context, records interface{}, queriers ...rel.Querier) (int, error) {
+	query := rel.Build("", queriers...)
+	for _, mfca := range fca {
+		if fetchContext(ctx) == mfca.ctxData && matchQuery(mfca.argQuery, query) {
+			if mfca.argRecords != nil {
+				reflect.ValueOf(records).Elem().Set(reflect.ValueOf(mfca.argRecords))
+			}
+
+			return mfca.retCount, mfca.retError
+		}
+	}
+
+	panic("TODO: Query doesn't match")
+}
+
+// MockFindAndCountAll asserts and simulate find and count all function for test.
+type MockFindAndCountAll struct {
+	ctxData    ctxData
+	argQuery   rel.Query
+	argRecords interface{}
+	retCount   int
+	retError   error
 }
 
 // Result sets the result of this query.
-func (fa *FindAndCountAll) Result(records interface{}, count int) {
-	fa.Arguments[1] = mock.AnythingOfType(fmt.Sprintf("*%T", records))
-
-	fa.Run(func(args mock.Arguments) {
-		reflect.ValueOf(args[1]).Elem().Set(reflect.ValueOf(records))
-	}).Return(count, nil)
+func (mfca *MockFindAndCountAll) Result(result interface{}, count int) {
+	mfca.argQuery.Table = rel.NewCollection(result, true).Table()
+	mfca.argRecords = result
+	mfca.retCount = count
 }
 
 // Error sets error to be returned.
-func (fa *FindAndCountAll) Error(err error) {
-	fa.Return(0, err)
+func (mfca *MockFindAndCountAll) Error(err error) {
+	mfca.retError = err
 }
 
 // ConnectionClosed sets this error to be returned.
-func (fa *FindAndCountAll) ConnectionClosed() {
-	fa.Error(ErrConnectionClosed)
-}
-
-// ExpectFindAndCountAll to be called with given field and queries.
-func ExpectFindAndCountAll(r *Repository, queriers []rel.Querier) *FindAndCountAll {
-	return &FindAndCountAll{
-		Expect: newExpect(r, "FindAndCountAll",
-			[]interface{}{r.ctxData, mock.Anything, queriers},
-			[]interface{}{0, nil},
-		),
-	}
+func (mfca *MockFindAndCountAll) ConnectionClosed() {
+	mfca.Error(ErrConnectionClosed)
 }

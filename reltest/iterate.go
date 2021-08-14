11d5db60a30932adbbe0c574b1607d435f208653
Fs02 <surya.asriadie@gmail.com>
2021-08-14 18:22:32 +0900

    rewrite iterate

--- reltest/iterate.go
+++ reltest/iterate.go
@@ -1,76 +1,91 @@
 package reltest
 
 import (
+	"context"
 	"io"
 	"reflect"
 
 	"github.com/go-rel/rel"
 )
 
+type iterate []*MockIterate
+
+func (i *iterate) register(ctxData ctxData, query rel.Query, options ...rel.IteratorOption) *MockIterate {
+	mi := &MockIterate{ctxData: ctxData, argQuery: query, argOptions: options}
+	*i = append(*i, mi)
+	return mi
+}
+
+func (i iterate) execute(ctx context.Context, query rel.Query, options ...rel.IteratorOption) rel.Iterator {
+	for _, mi := range i {
+		if fetchContext(ctx) == mi.ctxData &&
+			reflect.DeepEqual(mi.argOptions, options) &&
+			matchQuery(mi.argQuery, query) {
+			return mi
+		}
+	}
+
+	panic("TODO: Query doesn't match")
+}
+
 type data interface {
 	Len() int
 	Get(index int) *rel.Document
 }
 
-type iterator struct {
-	current int
-	data    data
-	err     error
+// MockIterate asserts and simulate Delete function for test.
+type MockIterate struct {
+	ctxData    ctxData
+	result     data
+	current    int
+	err        error
+	argQuery   rel.Query
+	argOptions []rel.IteratorOption
 }
 
-func (i iterator) Close() error {
-	return nil
-}
-
-func (i *iterator) Next(record interface{}) error {
-	if i.err != nil {
-		return i.err
-	}
-
-	if i.data == nil || i.current == i.data.Len() {
-		return io.EOF
-	}
-
-	var (
-		doc = i.data.Get(i.current)
-	)
-
-	reflect.ValueOf(record).Elem().Set(doc.ReflectValue())
-
-	i.current++
-	return nil
-}
-
-// Iterate asserts and simulate iterate function for test.
-type Iterate iterator
-
-// Error sets error to be returned.
-func (i *Iterate) Error(err error) {
-	i.err = err
-}
-
-// ConnectionClosed sets this error to be returned.
-func (i *Iterate) ConnectionClosed() {
-	i.Error(ErrConnectionClosed)
-}
-
-// Result sets the result of this query.
-func (i *Iterate) Result(records interface{}) {
-	rt := reflect.TypeOf(records)
+// Result sets the result of preload.
+func (mi *MockIterate) Result(result interface{}) {
+	rt := reflect.TypeOf(result)
 	if rt.Kind() == reflect.Ptr {
 		rt = rt.Elem()
 	}
 
 	if rt.Kind() == reflect.Slice {
-		i.data = rel.NewCollection(records, true)
+		mi.result = rel.NewCollection(result, true)
 	} else {
-		i.data = rel.NewDocument(records, true)
+		mi.result = rel.NewDocument(result, true)
 	}
 }
 
-// ExpectIterate to be called.
-func ExpectIterate(r *Repository, query rel.Query, options []rel.IteratorOption) *Iterate {
-	iterate := &Iterate{}
-	r.mock.On("Iterate", r.ctxData, query, options).Return(iterate).Once()
-	return iterate
+// Error sets error to be returned.
+func (mi *MockIterate) Error(err error) {
+	mi.err = err
+}
+
+// ConnectionClosed sets this error to be returned.
+func (mi *MockIterate) ConnectionClosed() {
+	mi.Error(ErrConnectionClosed)
+}
+
+func (mi MockIterate) Close() error {
+	return nil
+}
+
+func (mi *MockIterate) Next(record interface{}) error {
+	if mi.err != nil {
+		return mi.err
+	}
+
+	if mi.result == nil || mi.current == mi.result.Len() {
+		return io.EOF
+	}
+
+	var (
+		doc = mi.result.Get(mi.current)
+	)
+
+	reflect.ValueOf(record).Elem().Set(doc.ReflectValue())
+
+	mi.current++
+	return nil
 }

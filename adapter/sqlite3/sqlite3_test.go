c242f977e419d6acb52fa69da8c53c9f5c19b32d
Fs02 <surya.asriadie@gmail.com>
2021-08-18 20:47:19 +0900

    revert commented codes

--- adapter/sqlite3/sqlite3_test.go
+++ adapter/sqlite3/sqlite3_test.go
@@ -36,62 +36,62 @@ func TestAdapter_specs(t *testing.T) {
 	specs.Migrate(t, repo, specs.SkipDropColumn)
 
 	// Query Specs
-	// specs.Query(t, repo)
-	// specs.QueryJoin(t, repo)
-	// specs.QueryWhereSubQuery(t, repo, specs.SkipAllAndAnyKeyword)
-	// specs.QueryNotFound(t, repo)
+	specs.Query(t, repo)
+	specs.QueryJoin(t, repo)
+	specs.QueryWhereSubQuery(t, repo, specs.SkipAllAndAnyKeyword)
+	specs.QueryNotFound(t, repo)
 
-	// // Preload specs
-	// specs.PreloadHasMany(t, repo)
-	// specs.PreloadHasManyWithQuery(t, repo)
-	// specs.PreloadHasManySlice(t, repo)
-	// specs.PreloadHasOne(t, repo)
-	// specs.PreloadHasOneWithQuery(t, repo)
-	// specs.PreloadHasOneSlice(t, repo)
-	// specs.PreloadBelongsTo(t, repo)
-	// specs.PreloadBelongsToWithQuery(t, repo)
-	// specs.PreloadBelongsToSlice(t, repo)
+	// Preload specs
+	specs.PreloadHasMany(t, repo)
+	specs.PreloadHasManyWithQuery(t, repo)
+	specs.PreloadHasManySlice(t, repo)
+	specs.PreloadHasOne(t, repo)
+	specs.PreloadHasOneWithQuery(t, repo)
+	specs.PreloadHasOneSlice(t, repo)
+	specs.PreloadBelongsTo(t, repo)
+	specs.PreloadBelongsToWithQuery(t, repo)
+	specs.PreloadBelongsToSlice(t, repo)
 
-	// // Aggregate Specs
-	// specs.Aggregate(t, repo)
+	// Aggregate Specs
+	specs.Aggregate(t, repo)
 
-	// // Insert Specs
-	// specs.Insert(t, repo)
-	// specs.InsertHasMany(t, repo)
-	// specs.InsertHasOne(t, repo)
-	// specs.InsertBelongsTo(t, repo)
-	// specs.Inserts(t, repo)
-	// specs.InsertAll(t, repo)
-	// // specs.InsertAllPartialCustomPrimary(t, repo) - not supported
+	// Insert Specs
+	specs.Insert(t, repo)
+	specs.InsertHasMany(t, repo)
+	specs.InsertHasOne(t, repo)
+	specs.InsertBelongsTo(t, repo)
+	specs.Inserts(t, repo)
+	specs.InsertAll(t, repo)
+	// specs.InsertAllPartialCustomPrimary(t, repo) - not supported
 
-	// // Update Specs
-	// specs.Update(t, repo)
-	// specs.UpdateNotFound(t, repo)
-	// specs.UpdateHasManyInsert(t, repo)
-	// specs.UpdateHasManyUpdate(t, repo)
-	// specs.UpdateHasManyReplace(t, repo)
-	// specs.UpdateHasOneInsert(t, repo)
-	// specs.UpdateHasOneUpdate(t, repo)
-	// specs.UpdateBelongsToInsert(t, repo)
-	// specs.UpdateBelongsToUpdate(t, repo)
-	// specs.UpdateAtomic(t, repo)
-	// specs.Updates(t, repo)
-	// specs.UpdateAny(t, repo)
+	// Update Specs
+	specs.Update(t, repo)
+	specs.UpdateNotFound(t, repo)
+	specs.UpdateHasManyInsert(t, repo)
+	specs.UpdateHasManyUpdate(t, repo)
+	specs.UpdateHasManyReplace(t, repo)
+	specs.UpdateHasOneInsert(t, repo)
+	specs.UpdateHasOneUpdate(t, repo)
+	specs.UpdateBelongsToInsert(t, repo)
+	specs.UpdateBelongsToUpdate(t, repo)
+	specs.UpdateAtomic(t, repo)
+	specs.Updates(t, repo)
+	specs.UpdateAny(t, repo)
 
 	// Delete specs
-	// specs.Delete(t, repo)
-	// specs.DeleteBelongsTo(t, repo)
-	// specs.DeleteHasOne(t, repo)
+	specs.Delete(t, repo)
+	specs.DeleteBelongsTo(t, repo)
+	specs.DeleteHasOne(t, repo)
 	specs.DeleteHasMany(t, repo)
-	// specs.DeleteAll(t, repo)
-	// specs.DeleteAny(t, repo)
+	specs.DeleteAll(t, repo)
+	specs.DeleteAny(t, repo)
 
-	// // Constraint specs
-	// // - foreign key constraint is not supported because of lack of information in the error message.
-	// specs.UniqueConstraintOnInsert(t, repo)
-	// specs.UniqueConstraintOnUpdate(t, repo)
-	// specs.CheckConstraintOnInsert(t, repo)
-	// specs.CheckConstraintOnUpdate(t, repo)
+	// Constraint specs
+	// - foreign key constraint is not supported because of lack of information in the error message.
+	specs.UniqueConstraintOnInsert(t, repo)
+	specs.UniqueConstraintOnUpdate(t, repo)
+	specs.CheckConstraintOnInsert(t, repo)
+	specs.CheckConstraintOnUpdate(t, repo)
 }
 
 func TestAdapter_Transaction_commitError(t *testing.T) {

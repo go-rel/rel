1388ca55bdbd8265e40573ed0ccb9eaad9cb56e2
Lauris BH <lauris@nix.lv>
2021-10-20 14:24:26 +0300

    Remove unneeded logic and add comments

--- join_query.go
+++ join_query.go
@@ -15,20 +15,14 @@ func (jq JoinQuery) Build(query *Query) {
 	query.JoinQuery = append(query.JoinQuery, jq)
 }
 
-// NewJoinWith query with custom join mode, table and field.
+// NewJoinWith query with custom join mode, table, field and additional filters with AND condition.
 func NewJoinWith(mode string, table string, from string, to string, filter ...FilterQuery) JoinQuery {
-	var fq FilterQuery
-	if len(filter) == 1 {
-		fq = filter[0]
-	} else if len(filter) > 1 {
-		fq = And(filter...)
-	}
 	return JoinQuery{
 		Mode:   mode,
 		Table:  table,
 		From:   from,
 		To:     to,
-		Filter: fq,
+		Filter: And(filter...),
 	}
 }
 
@@ -50,47 +44,47 @@ func NewJoin(table string, filter ...FilterQuery) JoinQuery {
 	return NewJoinWith("JOIN", table, "", "", filter...)
 }
 
-// NewJoinOn table with given field.
+// NewJoinOn table with given field and optional additional filter.
 func NewJoinOn(table string, from string, to string, filter ...FilterQuery) JoinQuery {
 	return NewJoinWith("JOIN", table, from, to, filter...)
 }
 
-// NewInnerJoin with given table.
+// NewInnerJoin with given table and optional filter.
 func NewInnerJoin(table string, filter ...FilterQuery) JoinQuery {
 	return NewInnerJoinOn(table, "", "", filter...)
 }
 
-// NewInnerJoinOn table with given field.
+// NewInnerJoinOn table with given field and optional additional filter.
 func NewInnerJoinOn(table string, from string, to string, filter ...FilterQuery) JoinQuery {
 	return NewJoinWith("INNER JOIN", table, from, to, filter...)
 }
 
-// NewLeftJoin with given table.
+// NewLeftJoin with given table and optional filter.
 func NewLeftJoin(table string, filter ...FilterQuery) JoinQuery {
 	return NewLeftJoinOn(table, "", "", filter...)
 }
 
-// NewLeftJoinOn table with given field.
+// NewLeftJoinOn table with given field and optional additional filter.
 func NewLeftJoinOn(table string, from string, to string, filter ...FilterQuery) JoinQuery {
 	return NewJoinWith("LEFT JOIN", table, from, to, filter...)
 }
 
-// NewRightJoin with given table.
+// NewRightJoin with given table and optional filter.
 func NewRightJoin(table string, filter ...FilterQuery) JoinQuery {
 	return NewRightJoinOn(table, "", "", filter...)
 }
 
-// NewRightJoinOn table with given field.
+// NewRightJoinOn table with given field and optional additional filter.
 func NewRightJoinOn(table string, from string, to string, filter ...FilterQuery) JoinQuery {
 	return NewJoinWith("RIGHT JOIN", table, from, to, filter...)
 }
 
-// NewFullJoin with given table.
+// NewFullJoin with given table and optional filter.
 func NewFullJoin(table string, filter ...FilterQuery) JoinQuery {
 	return NewFullJoinOn(table, "", "", filter...)
 }
 
-// NewFullJoinOn table with given field.
+// NewFullJoinOn table with given field and optional additional filter.
 func NewFullJoinOn(table string, from string, to string, filter ...FilterQuery) JoinQuery {
 	return NewJoinWith("FULL JOIN", table, from, to, filter...)
 }
